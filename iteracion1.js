4e254e7f9dce37e77bd3030ebeb9535a19895bbf
ricardograna <ricardogranarosales@gmail.com>
2022-10-14 00:22:17 +0200

    iteracion2

--- iteracion1.js
+++ iteracion1.js
@@ -1,6 +1,21 @@
 //1.1
-fetch("https://api.agify.io?name=michael")
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-  });
+//fetch("https://api.agify.io?name=michael")
+///  .then((data) => console.log(data));
+
+   //1.2
+   const baseUrl = "https://api.nationalize.io";
+
+   const search = (event) => {
+     const input$$ = document.querySelector("input");
+   
+     fetch(baseUrl + "?name=" + input$$.value)
+       .then((res) => res.json())
+       .then((res) => {
+         console.log(res);
+       });
+   };
+   
+   const button$ = document.querySelector("button");
+   
+   button$.addEventListener("click", search);
+  
